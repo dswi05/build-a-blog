5af94a9bf8f37b5b5646d2b0a648ef3eac19f5c1
Douglas Swiatocha <dswiatocha@gmail.com>
2019-02-07 20:34:40 -0600

    started buildablog

---
+++ build-a-blog/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(5000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+    return render_template('blog.html')
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_post():
+    return render_template('newpost.html')
+
+if __name__ == '__main__':
+    app.run()
